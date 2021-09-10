525b873d2bb7d1111bc47008cc6f92794cb7c42c
Hardeep Singh <hardeep.singh@jungleworks.com>
2021-09-10 22:57:07 +0530

    TestCase No.2:- "test_load_deliversConnectivityErrorOnClientError"  passed

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,7 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
-			completion(.failure(NSError(domain: "Testing", code: -1, userInfo: nil)))
+			completion(.failure(Error.connectivity))
 		}
 	}
 }
